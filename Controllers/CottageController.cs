a473ee9d3837ca852fa41ddc119f54a3cbadceec
JoelLamsi <joel.lamsijarvi@live.com>
2020-09-16 01:14:03 +0300

    Added my cottage project

---
+++ Controllers/CottageController.cs
@@ -0,0 +1,72 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using CottageApp.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CottageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CottageController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CottageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<Cottage> GetCottages()
+        {
+            var cottages = new List<Cottage>()
+            {
+                
+            };
+
+            //_context.Cottages.ToList();
+
+            return cottages;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCottageAsync(int id)
+        {
+            var cottage = await _context.Cottages.FindAsync(id);
+            return Ok(cottage);
+        }
+
+        // [HttpPut]
+        // public async Task<IActionResult> UpdateCottageAsync(int id, Cottage cottage)
+        // {
+        //     var c = _context.Cottages.Find(id);
+        //     if (c == null)
+        //     {
+        //         return BadRequest();
+        //     }
+
+        //     _context.Cottages.Update(cottage);
+        //     return Ok();
+        // }
+
+        [HttpPost]
+        public async Task CreateCottageAsync(Cottage input)
+        {
+            var cottage = new Cottage
+            {
+                Title = input.Title,
+                Description = input.Description,
+                CostPerDay = input.CostPerDay
+            };
+
+            await _context.Cottages.AddAsync(cottage);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
