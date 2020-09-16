8346d22f91259a42463c060a6ea99bb588f1c775
JoelLamsi <joel.lamsijarvi@live.com>
2020-09-16 15:18:58 +0300

    Added Backend

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{ 
+        //    options.UseSqlite("Data Source=cottage-app.db");
+        //}
+
+        //public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Cottage> Cottages { get; set; }
+        //public DbSet<Person> People { get; set; }
+        //public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ Backend/Data/Cottage.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Data
+{
+    public class Cottage
+    {
+        public int Id { get; set; }
+        [Required, StringLength(100, ErrorMessage = "Title must be between 5 and 100 character", MinimumLength = 5)]
+        public string Title { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        [NotMapped]
+        public int[] Ratings { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Rooms { get; set; }
+        public bool IsSauna { get; set; }
+        public bool IsElectricity { get; set; }
+
+        public float AvgRating 
+        {
+            get { return AvgRatings(); }
+        }
+        [Required]
+        public decimal CostPerDay { get; set; }
+
+        private float AvgRatings()
+        {
+            var count = Ratings.Length;
+            var sum = 0;
+            foreach (var i in Ratings)
+            {
+                sum += i;
+            }
+            return sum / count;
+        }
+    }
+}
